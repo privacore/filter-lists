3e2a086f84d2d140a990ff8c405e1e83571159b6
Brian Rasmusson <br@privacore.com>
2018-04-13 22:52:56 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # filter-lists
 Some filter lists managed by Privacore, used in Findx Privacy Control
+
+This repository is work in progress..
+
---
+++ sign.sh
@@ -0,0 +1,6 @@
+python addChecksum.py < facebook/antifacebook.txt >tmp.txt
+mv tmp.txt facebook/antifacebook.txt
+
+python addChecksum.py < google/antigoogle.txt >tmp.txt
+mv tmp.txt google/antigoogle.txt
+
