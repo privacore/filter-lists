7ff428f4ce36eef7f7f0fddf02108f100c823a4e
Brian Rasmusson <br@privacore.com>
2018-06-22 11:41:07 +0200

    added adult blocklist

--- sign.sh
+++ sign.sh
@@ -4,3 +4,6 @@ mv tmp.txt facebook/antifacebook.txt
 python addChecksum.py < google/antigoogle.txt >tmp.txt
 mv tmp.txt google/antigoogle.txt
 
+python addChecksum.py < adult/findx-adult.txt >tmp.txt
+mv tmp.txt adult/findx-adult.txt
+
